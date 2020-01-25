2b4acb69166dfe907b96249b0a2261e4e3937a2a
Ali Al-Bahrani <alibahrani@mac.com>
2020-01-25 13:17:55 -0500

    Updates

---
+++ precache-manifest.32e4d19eff258725f6d9257f3b4b602f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e469fbf43eb9db80814eed3f7bd12907",
+    "url": "/sample-monster-trial/index.html"
+  },
+  {
+    "revision": "cdea813ec1cc5630b5ff",
+    "url": "/sample-monster-trial/static/css/main.144a501c.chunk.css"
+  },
+  {
+    "revision": "c4f19fda592d5bb05bfa",
+    "url": "/sample-monster-trial/static/js/2.532c7c9c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/sample-monster-trial/static/js/2.532c7c9c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cdea813ec1cc5630b5ff",
+    "url": "/sample-monster-trial/static/js/main.20991791.chunk.js"
+  },
+  {
+    "revision": "f9519011108348c3a798",
+    "url": "/sample-monster-trial/static/js/runtime-main.b204426b.js"
+  }
+]);
